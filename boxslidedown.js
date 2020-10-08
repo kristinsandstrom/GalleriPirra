e36c3b4b92904532c45051b8c908f9cd03e67df3
kristinsandstrom <54585976+kristinsandstrom@users.noreply.github.com>
2020-10-08 20:53:29 +0200

    Add files via upload

---
+++ boxslidedown.js
@@ -0,0 +1,25 @@
+
+
+//SLIDE UP AND DOWN
+		
+	$(document).ready(function(){
+		$(".info").click (function(){
+			$("#first").slideToggle("slow");	
+	    });		
+	});	
+	
+	
+	$(document).ready(function(){
+		$(".exhibition").click (function(){
+			$("#second").slideToggle("slow");	
+	    });		
+	});	
+	
+	
+	$(document).ready(function(){
+		$(".current").click (function(){
+			$("#third").slideToggle("slow");	
+	    });		
+	});	
+	
+	
